887a1477ffc3230980c3dd1f61f2233ce19023bf
rbool <randomboolean@gmail.com>
2017-01-19 14:40:01 +0100

    added lisence and fixed indents

--- src/main/java/io/warp10/tutorial/HELLOWARP10.java
+++ src/main/java/io/warp10/tutorial/HELLOWARP10.java
@@ -1,3 +1,19 @@
+//
+//   Copyright 2016  Cityzen Data
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+
 package io.warp10.tutorial;
 
 import io.warp10.script.NamedWarpScriptFunction;
@@ -7,33 +23,33 @@ import io.warp10.script.WarpScriptStackFunction;
 
 public class HELLOWARP10 extends NamedWarpScriptFunction implements WarpScriptStackFunction {
 
-    public HELLOWARP10(String name) {
+  public HELLOWARP10(String name) {
         super(name);
     }
 
-    public Object apply(WarpScriptStack stack) throws WarpScriptException {
+  public Object apply(WarpScriptStack stack) throws WarpScriptException {
 
-        //
-        // Get params
-        //
+    //
+    // Get params
+    //
 
-        Object firstText = stack.pop();
-        Object secondText = stack.pop();
+    Object firstText = stack.pop();
+    Object secondText = stack.pop();
 
-        if (!(firstText instanceof String)) {
-            throw new WarpScriptException("First param must be a String");
-        }
-        if (!(secondText instanceof String)) {
-            throw new WarpScriptException("Second param must be a String");
-        }
-        try {
-            stack.push(firstText.toString() + ' ' + secondText.toString());
-        } catch (WarpScriptException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-            throw new WarpScriptException("Stack exception" + e.getMessage() + e.getStackTrace().toString());
-        }
-
-        return stack;
+    if (!(firstText instanceof String)) {
+      throw new WarpScriptException("First param must be a String");
     }
+    if (!(secondText instanceof String)) {
+      throw new WarpScriptException("Second param must be a String");
+    }
+    try {
+      stack.push(firstText.toString() + ' ' + secondText.toString());
+    } catch (WarpScriptException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+      throw new WarpScriptException("Stack exception" + e.getMessage() + e.getStackTrace().toString());
+    }
+
+    return stack;
+  }
 }
--- src/main/java/io/warp10/tutorial/TutorialExtension.java
+++ src/main/java/io/warp10/tutorial/TutorialExtension.java
@@ -1,3 +1,19 @@
+//
+//   Copyright 2016  Cityzen Data
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+
 package io.warp10.tutorial;
 
 import io.warp10.warp.sdk.WarpScriptExtension;
@@ -7,14 +23,14 @@ import java.util.Map;
 
 public class TutorialExtension extends WarpScriptExtension {
 
-    private final Map<String,Object> functions;
+  private final Map<String,Object> functions;
 
-    public TutorialExtension() {
-        this.functions = new HashMap<String, Object>();
-        functions.put("HELLOWARP10", new HELLOWARP10("HELLOWARP10"));
-    }
+  public TutorialExtension() {
+    this.functions = new HashMap<String, Object>();
+    functions.put("HELLOWARP10", new HELLOWARP10("HELLOWARP10"));
+  }
 
-    public Map<String, Object> getFunctions() {
+  public Map<String, Object> getFunctions() {
         return functions;
     }
 }
